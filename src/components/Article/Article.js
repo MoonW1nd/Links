69ebf3f67e154076a6b250add794fbcbac9c25a0
Alexander Ivankov <alexanderivankoff@gmail.com>
2018-03-21 22:28:35 +0300

    Add shoudComponentUpdate in Article component

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -2,35 +2,46 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import CommentList from '../CommentList/CommentList';
 
-function Article({ article, isOpen, toggleOpen }) {
-  return (
-    <div>
-      <h3>{article.title}</h3>
-      <button onClick = { toggleOpen }>
-        { isOpen ? 'Close' : 'Open' }
-      </button>
-      { getContent(article, isOpen) }
-    </div>
-  );
-}
 
-function getContent(article, isOpen) {
-  if (!isOpen) return null;
-  return <section>
-    {article.text}
-    <CommentList comments = { article.comments }/>
-  </section>;
+export default class Article extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    return nextProps.isOpen !== this.props.isOpen;
+  }
+
+  render() {
+    const { article, isOpen, toggleOpen } = this.props;
+    console.log('update article');
+    return (
+      <div>
+        <h3>{ article.title }</h3>
+        <button onClick = { toggleOpen }>
+          { isOpen ? 'Close' : 'Open' }
+        </button>
+        { this.getContent() }
+      </div>
+    );
+  }
+
+  getContent = () => {
+    const { isOpen } = this.props;
+    const { comments } = this.props.article;
+    const content = this.props.article.text;
+    if (!isOpen) return null;
+    return <section>
+      {content}
+      <CommentList comments = { comments }/>
+    </section>;
+  }
 }
 
 
 Article.propTypes = {
+  id: PropTypes.string.isRequired,
   article: PropTypes.shape({
-    id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     text: PropTypes.string,
+    comments: PropTypes.array,
   }),
   isOpen: PropTypes.bool,
   toggleOpen: PropTypes.func,
 };
-
-export default Article;
