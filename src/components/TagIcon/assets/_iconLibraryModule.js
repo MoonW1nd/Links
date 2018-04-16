ccd6d05bdb4efe7945db60fc6de834686608473f
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2018-04-16 21:51:53 +0300

    Update styles for Header and add iconLibrary in tagIcon

---
+++ src/components/TagIcon/assets/_iconLibraryModule.js
@@ -0,0 +1,11 @@
+import nodeJS from './nodeJS.svg';
+import javaScript from './javaScript.svg';
+import gulp from './gulp.svg';
+import npm from './npm.svg';
+
+export default {
+  nodeJS,
+  javaScript,
+  gulp,
+  npm,
+};
--- src/components/TagIcon/index.js
+++ src/components/TagIcon/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import iconLibrary from './assets/_iconLibraryModule';
 
 export default function TagImage(props) {
+  let img = iconLibrary[props.tagName];
+  if (img === undefined) img = '';
   return <img
     className = {props.tagName ? `TagIcon TagIcon_${props.tagName}` : 'TagName'}
-    src = 'assets/nodeJS.svg'
+    src = {img}
     alt = {props.tagName ? `tag-${props.tagName}` : 'tag'}
   />;
 }
