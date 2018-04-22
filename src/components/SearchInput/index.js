d39229720cd922b21921089fbc36b39895c426c8
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2018-04-22 18:01:00 +0300

    SearchInput add styles and design modify

--- src/components/SearchInput/index.js
+++ src/components/SearchInput/index.js
@@ -1,14 +1,21 @@
-
 import React from 'react';
+import './style.scss';
+
 export default class SearchInput extends React.Component {
   state = {
     searchValue: '',
   }
 
   render() {
-    return <div>
-      <label>Search:<input type = 'text' value = { this.state.value } onChange = { this.handeSearchInput }/></label>
-    </div>;
+    return <label className='SearchInput Header-SearchInput'>
+      <input
+        type = 'text'
+        value = { this.state.searchValue }
+        onChange = { this.handeSearchInput }
+        placeholder = 'Поиск'
+        className = 'Input Input_type_search'
+      />
+    </label>;
   }
 
   handeSearchInput = (ev) => {
