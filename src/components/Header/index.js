8b629765696e7848bbffe2b810f75aaa0aa72e55
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2018-05-02 16:14:50 +0300

    Some style fixes and temorary hide search field

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Logo from '../Logo';
 import './style.scss';
-import SearchInput from '../SearchInput';
+// import SearchInput from '../SearchInput';
 import LogoButton from '../Buttons/LogoButton';
 
 export default function Header() {
   return <header className='Header'>
-    <SearchInput/>
+    {/* <SearchInput/> */}
     <Logo/>
     <div className='Header-ButtonsWrapper'>
       <LogoButton logoName='AlexanderIvankovLogo' link='http://alexanderivankov.ru/'/>
