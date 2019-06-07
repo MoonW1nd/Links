af05b3af0f34d0050b2319b28bd51b08b4427e99
Alexander Ivankov <moonw1nd@yandex-team.ru>
2019-06-07 21:31:58 +0300

    feat: update logo Header design

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
 import './style.scss';
 
-export default function Logo() {
-  return <div className='Logo'>
-    <u>Links</u>
+export default function Logo({ className }) {
+  return <div className={`Logo ${className}`}>
+    <span>links</span>
   </div>;
 }
+
+Logo.propTypes = {
+  className: PropTypes.string,
+};
